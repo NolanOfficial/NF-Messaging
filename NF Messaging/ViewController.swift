2bb1884755dc196941f804039b291e84cfbda9b3
Nolan Fuchs <nolanfuchs@nolans-mbp.domain>
2018-12-03 14:18:11 -0700

    Initial Commit

---
+++ NF Messaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NF Messaging
+//
+//  Created by Nolan Fuchs on 12/3/18.
+//  Copyright © 2018 Nolan Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
